364023d3bda6d03b3017bcab050b3703b1dc84ea
Sjur Føyen <sjur.foyen@gmail.com>
2024-02-05 14:59:32 +0100

    Updated assignment_model.py (the SMIB) and tidied up assignment_base_script.py.

--- examples/dyn_sim/assignment_base_script.py
+++ examples/dyn_sim/assignment_base_script.py
@@ -2,8 +2,6 @@ from collections import defaultdict
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import time
-import sys
 import tops.dynamic as dps
 import tops.solvers as dps_sol
 import importlib
@@ -12,42 +10,28 @@ importlib.reload(dps)
 
 if __name__ == '__main__':
 
-    # Load model
-    import tops.ps_models.assignment_model as model_data
+    # region Model loading and initialisation stage
+    import tops.ps_models.assignment3_model as model_data
     model = model_data.load()
+    ps = dps.PowerSystemModel(model=model)  # Load into a PowerSystemModel object
 
-    # Power system model
-    ps = dps.PowerSystemModel(model=model)
-    # ps.use_numba = True
-    # Power flow calculation
-    ps.power_flow()
-    # Initialization
-    ps.init_dyn_sim()
-    #
-    np.max(ps.ode_fun(0.0, ps.x0))
-    # Specify simulation time
-    #
-    t_end = 5
-    x0 = ps.x0.copy()
-    # Add small perturbation to initial angle of first generator
-    # x0[ps.gen_mdls['GEN'].state_idx['angle'][0]] += 1
-    #
-    # Solver
-    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x0, t_end, max_step=5e-3)
+    ps.power_flow()  # Power flow calculation
 
-    # Define other variables to plot
-    P_m_stored = []
-    P_e_stored = []
-    E_f_stored = []
-    v_bus = []
-    I_stored = []
-    # Initialize simulation
+    ps.init_dyn_sim()  # Initialise dynamic variables
+    x0 = ps.x0.copy()  # Initial states
+
     t = 0
     result_dict = defaultdict(list)
-    t_0 = time.time()
-    # ps.build_y_bus_red(ps.buses['name'])
-    #ps.build_y_bus(['B2'])
+    t_end = 5  # Simulation time
 
+    # Possible to add perturbation directly through states:
+    # x0[ps.gen_mdls['GEN'].state_idx['angle'][0]] += 1  #initial angle of first generator
+
+    # Solver
+    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x0, t_end, max_step=5e-3)
+    # endregion
+
+    # region Print initial conditions
     v_bus_mag = np.abs(ps.v_0)
     v_bus_angle = ps.v_0.imag / v_bus_mag
     #
@@ -57,45 +41,54 @@ if __name__ == '__main__':
     print(' ')
     print('Voltage magnitudes  (kV) = ', v_bus_mag*[10, 245, 245])
     print(' ')
-    # print(ps.v_g)
-    #
-    print('state description: ', ps.state_desc)
+    print('state description: \n', ps.state_desc)
     print('Initial values on all state variables (G1 and IB) :')
     print(x0)
     print(' ')
-    # Run simulation
+    # endregion
+
+    # region Runtime variables
+    # Additional plot variables
+    P_m_stored = []
+    P_e_stored = []
+    E_f_stored = []
+    v_bus = []
+    I_stored = []
 
     event_flag1 = True
-    event_flag2 = True
-    #event_flag3 = True
+    sc_bus_idx = ps.gen['GEN'].bus_idx_red['terminal'][0]
+    # endregion
 
+    # Simulation loop starts here!
     while t < t_end:
-        # print(t)
-        #v_bus_full = ps.red_to_full.dot(ps.v_red)
-		
         result = sol.step()
         x = sol.y
         v = sol.v
         t = sol.t
 
-        # Store result
+        # Short circuit
+        if t >= 1 and t <= 1.05:
+            ps.y_bus_red_mod[(sc_bus_idx,) * 2] = 1e6
+        else:
+            ps.y_bus_red_mod[(sc_bus_idx,) * 2] = 0
+
+        # region Store variables
         result_dict['Global', 't'].append(sol.t)
         [result_dict[tuple(desc)].append(state) for desc, state in zip(ps.state_desc, x)]
-        # Legger til nye outputs
+        # Store additional variables
         P_m_stored.append(ps.gen['GEN'].P_m(x, v).copy())
         P_e_stored.append(ps.gen['GEN'].P_e(x, v).copy())
         E_f_stored.append(ps.gen['GEN'].E_f(x, v).copy())
 
         I_gen = ps.y_bus_red_full[0, 1] * (v[0] - v[1])
         I_stored.append(np.abs(I_gen))
+        # endregion
 
-    print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
 
     # Convert dict to pandas dataframe
-    index = pd.MultiIndex.from_tuples(result_dict)
-    result = pd.DataFrame(result_dict, columns=index)
+    result = pd.DataFrame(result_dict, columns=pd.MultiIndex.from_tuples(result_dict))
 
-    #Plot speed, angle and electric power as a function of time
+    # region Plotting
     fig, ax = plt.subplots(3)
     fig.suptitle('Generator speed, power angle and electric power')
     ax[0].plot(result[('Global', 't')], result.xs(key='speed', axis='columns', level=1))
@@ -106,12 +99,9 @@ if __name__ == '__main__':
     ax[2].set_ylabel('Elec. power (p.u.)')
     ax[2].set_xlabel('time (s)')
 
-
-
     plt.figure()
     plt.plot(result[('Global', 't')], np.array(E_f_stored))
     plt.xlabel('time (s)')
     plt.ylabel('E_q (p.u.)')
-    
-
     plt.show()
+    # endregion
